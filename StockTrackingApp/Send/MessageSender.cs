9c0d21b1f2d66327ffb7b0377dd6d5e7900ca037
Joe Waddington <38754787+JoeWaddington@users.noreply.github.com>
2021-01-18 10:30:32 +0000

    Send, StockTrackingApp and sIn file updated

---
+++ StockTrackingApp/Send/MessageSender.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Send
+{
+    class MessageSender
+    {
+        private readonly IModel _channel;
+        private string queueName = "";
+
+        public MessageSender(IModel channel)
+        {
+            _channel = channel;
+        }
+
+        public void BindQueue(string queueName)
+        {
+            _channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            _channel.QueueBind(queueName, "stocks", "");
+        }
+
+        public void DeclareQueue(string queueName) // declare and bind queue to the exchange
+        {
+            _channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            _channel.QueueBind(queueName, "stocks", "");
+        }
+
+        public void SendMessage(string message)
+        {
+            
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(exchange: "stocks", routingKey: "", basicProperties: null, body: body);
+            Console.WriteLine("Sent {0}", message);
+            
+        }
+    }
+}

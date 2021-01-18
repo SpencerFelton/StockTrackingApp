981cd2cb0dfdf7b0c35f20e2e7786b37762dc88d
Joe Waddington <38754787+JoeWaddington@users.noreply.github.com>
2021-01-18 10:46:55 +0000

    Pushing new solution file with folders
    - Hopefully fixed the solution file

---
+++ StockTrackingApp/ProducerForm/Default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ProducerForm._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:TextBox ID="updateStockTextbox" runat="server"></asp:TextBox>
+    <asp:Button ID="UpdateStockButton" runat="server" Text="Change Stock" OnClick="UpdateStockButton_Click" />
+
+</asp:Content>
---
+++ StockTrackingApp/ProducerForm/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Producer;
+using System.Threading.Tasks;
+
+
+namespace ProducerForm
+{
+    public partial class _Default : Page
+    {
+        static ProducerAPICaller2 prod = new ProducerAPICaller2();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UpdateStockButton_Click(object sender, EventArgs e)
+        {
+            string value = updateStockTextbox.Text;
+            Task.Run(() => SendAPIRequest(prod, value));
+
+            // Test connection
+            //string test = Task.Run(() => CheckConn(prod)).Result;
+            //Response.Write(test);
+        }
+
+        static async Task SendAPIRequest(ProducerAPICaller2 prod, string val)
+        {
+            await prod.SendStockValue(val);
+        }
+
+        static async Task<string> CheckConn(ProducerAPICaller2 prod)
+        {
+            return await prod.TestConn();
+        }
+    }
+}

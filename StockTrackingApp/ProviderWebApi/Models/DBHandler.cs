7f5413ab487045778d51c4d899d1b83d34f03cb6
JackBarlow <barlowj98@outlook.com>
2021-01-26 19:58:47 +0000

    Added full documentation to DBHandler

--- StockTrackingApp/ProviderWebApi/Models/DBHandler.cs
+++ StockTrackingApp/ProviderWebApi/Models/DBHandler.cs
@@ -16,6 +16,13 @@ namespace ProviderWebApi.Models
         private const int maxStockAbbrLength = 4;
 
         #region DATABASE MODIFIERS
+        /// <summary>
+        /// Creates a new <see cref="Stock"/> in the StockTracker database, then adds a new <see cref="PriceHistory"/> entry to ensure the stock has price data.
+        /// </summary>
+        /// <param name="name">The full company name of the stock.</param>
+        /// <param name="abbreviation">The shorthand abbreviation of the stock. 4 character limit.</param>
+        /// <param name="dateTime">The date and time when the price was accurate to.</param>
+        /// <param name="price">The price of the stock at the given date and time, in USD.</param>
         public static void AddStock(string name, string abbreviation, DateTime dateTime, decimal price)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -50,6 +57,10 @@ namespace ProviderWebApi.Models
                 entity.SaveChanges();
             }
         }
+        /// <summary>
+        /// Creates a new <see cref="Stock"/> to the StockTracker database, then adds a new <see cref="PriceHistory"/> entry to ensure the stock has price data.
+        /// </summary>
+        /// <param name="transitStock">The <see cref="TransitStock"/> containing all the stock and priceHistory information required.</param>
         public static void AddStock(TransitStock transitStock)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -75,6 +86,10 @@ namespace ProviderWebApi.Models
             }
         }
 
+        /// <summary>
+        /// Deletes a <see cref="Stock"/> and all its <see cref="PriceHistory"/> data from the StockTracker database.
+        /// </summary>
+        /// <param name="abbreviation">The abbreviation of the stock to be deleted.</param>
         public static void DeleteStock(string abbreviation)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -95,6 +110,10 @@ namespace ProviderWebApi.Models
                 }
             }
         }
+        /// <summary>
+        /// Removes a <see cref="Stock"/> and all its <see cref="PriceHistory"/> data from the StockTracker database.
+        /// </summary>
+        /// <param name="transitStock">A <see cref="TransitStock"/> containing the abbreviation of the stock to be deleted.</param>
         public static void DeleteStock(TransitStock transitStock)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -116,6 +135,12 @@ namespace ProviderWebApi.Models
             }
         }
 
+        /// <summary>
+        /// Creates a new <see cref="PriceHistory"/> entry for a specified stock.
+        /// </summary>
+        /// <param name="abbreviation">The abbreviation of the stock.</param>>
+        /// <param name="dateTime">The date and time when the price was accurate to.</param>
+        /// <param name="price">The price of the stock at the given date and time, in USD.</param>
         public static void UpdateStockPrice(string abbreviation, DateTime dateTime, decimal price)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -133,6 +158,10 @@ namespace ProviderWebApi.Models
                 entity.SaveChanges();
             }
         }
+        /// <summary>
+        /// Creates a new <see cref="PriceHistory"/> entry for a specified stock.
+        /// </summary>
+        /// <param name="transitStock">The <see cref="TransitStock"/> containing the abbreviation of the stock, the price and the associated date and time.</param>
         public static void UpdateStockPrice(TransitStock transitStock)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -153,6 +182,10 @@ namespace ProviderWebApi.Models
         #endregion
 
         #region TESTER SUPPLEMENTS
+        /// <summary>
+        /// Counts all the stocks in the Stocks table.
+        /// </summary>
+        /// <returns>The count/number</returns>
         public static int StockCount()
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -161,6 +194,16 @@ namespace ProviderWebApi.Models
             }
         }
 
+        /// <summary>
+        /// Checks if a stock exists in the database with the same parameters as those provided.
+        /// </summary>
+        /// <param name="name">The name of the stock.</param>
+        /// <param name="abbreviation">The abbreviation of the stock.</param>
+        /// <returns>An integer stating what whether no stock was found (0), a stock with the same abbreviation was found(1), a stock with the same name was found (2) or both (3).</returns>
+        /// <remarks>
+        /// Cannot distinguish between multiple stocks sharing the same name. Cannot distinguish between multiple stocks sharing the same abbreviation.
+        /// A return of 3 could mean the there was one stock with the given name and another stock with the given abbreviation OR one stock with both.
+        /// </remarks>
         public static int CheckStockExists(string name, string abbreviation)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -174,6 +217,10 @@ namespace ProviderWebApi.Models
         #endregion
 
         #region GETTERS - OBJECT(s) OR NULL(s) RETURNED WITH 1 EXCEPTION
+        /// <summary>
+        /// Reads all the <see cref="Stock"/>s from the Stocks table.
+        /// </summary>
+        /// <returns>An <see cref="Array{Stock}"/> containing all the <see cref="Stock"/>s in the Stocks table.</returns>
         public static Stock[] GetStocks()
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -182,6 +229,11 @@ namespace ProviderWebApi.Models
             }
         }
 
+        /// <summary>
+        /// Reads a particular <see cref="Stock"/> from the Stocks table.
+        /// </summary>
+        /// <param name="id">The id of the stock to be found.</param>
+        /// <returns>The particular <see cref="Stock"/> if it exists, otherwise null.</returns>
         public static Stock GetStock(int id)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -189,6 +241,11 @@ namespace ProviderWebApi.Models
                 return entity.Stocks.SingleOrDefault(s => s.id == id);
             }
         }
+        /// <summary>
+        /// Reads a particular <see cref="Stock"/> from the Stocks table.
+        /// </summary>
+        /// <param name="abbreviation">The abbreviation of the stock to be found.</param>
+        /// <returns>The particular <see cref="Stock"/> if it exists, otherwise null.</returns>
         public static Stock GetStock(string abbreviation)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -196,7 +253,12 @@ namespace ProviderWebApi.Models
                 return entity.Stocks.SingleOrDefault(s => s.abbr == abbreviation);
             }
         }
-        
+
+        /// <summary>
+        /// Reads a particular <see cref="Stock"/> from the Stocks table from the name.
+        /// </summary>
+        /// <param name="name">The name of the stock to be found.</param>
+        /// <returns>The particular <see cref="Stock"/> if it exists, otherwise null.</returns>
         public static Stock GetStockFromName(string name)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -204,7 +266,14 @@ namespace ProviderWebApi.Models
                 return entity.Stocks.SingleOrDefault(s => s.name == name);
             }
         }
-        
+
+        /// <summary>
+        /// Reads the most recent <see cref="PriceHistory"/> of a <see cref="Stock"/>.
+        /// </summary>
+        /// <param name="stock">The stock whom's latest price is to be found.</param>
+        /// <returns>The price of the <see cref="Stock"/> with the latest associated date and time.</returns>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ArgumentOutOfRangeException"/>
         public static decimal GetMostRecentStockPrice(Stock stock) // Throws exceptions // May be problematic if implemented
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -235,7 +304,12 @@ namespace ProviderWebApi.Models
                 return price;
             }
         }
-        
+
+        /// <summary>
+        /// Reads the most recent <see cref="PriceHistory"/> entry of a <see cref="Stock"/>.
+        /// </summary>
+        /// <param name="stock">The stock whom's latest price is to be found.</param>
+        /// <returns>The <see cref="PriceHistory"/> of the <see cref="Stock"/> if exists, or null.</returns>
         public static PriceHistory GetMostRecentStockPriceHistory(Stock stock)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -263,7 +337,11 @@ namespace ProviderWebApi.Models
                 return latestPriceHistory;
             }
         }
-        
+
+        /// <summary>
+        /// Reads all the <see cref="PriceHistory"/>s in the PriceHistory table.
+        /// </summary>
+        /// <returns>An <see cref="Array"/> containing all the <see cref="PriceHistory"/>s in the PriceHistory table.</returns>
         public static PriceHistory[] GetPriceHistories()
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -271,6 +349,11 @@ namespace ProviderWebApi.Models
                 return entity.PriceHistories.ToArray();
             }
         }
+        /// <summary>
+        /// Reads all the price histories in the PriceHistory table for a particular stock.
+        /// </summary>
+        /// <param name="stock">The stock whom's priceHistories are to be found.</param>
+        /// <returns>An <see cref="Array"/> containing all the <see cref="PriceHistory"/>s if the <see cref="Stock"/> exists, otherwise an empty array.</returns>
         public static PriceHistory[] GetPriceHistories(Stock stock)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())
@@ -282,6 +365,11 @@ namespace ProviderWebApi.Models
             }
         }
 
+        /// <summary>
+        /// Reads a particular entry from the PriceHistory table by the id of the entry.
+        /// </summary>
+        /// <param name="id">The id of the price history.</param>
+        /// <returns>The <see cref="PriceHistory"/> if exists, otherwise null.</returns>
         public static PriceHistory GetPriceHistory(int id)
         {
             using (StockTrackerEntities entity = new StockTrackerEntities())

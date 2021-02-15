a4cd8f6af129f39ee350c77a2447a05515d234bd
Alex Robinson <allexjamesrobinson@gmail.com>
2021-02-15 15:02:42 +0000

    Altered Data model
    Changed data model to reflect that on the Provider side. Less coupling between stocks and price histories to reflect the removal of TransitStock.

--- StockTrackingApp/SubscriberWebAPI/Models/StockTrackerModel.Context.cs
+++ StockTrackingApp/SubscriberWebAPI/Models/StockTrackerModel.Context.cs
@@ -13,10 +13,10 @@ namespace SubscriberWebAPI.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class StockTrackerEntities : DbContext
+    public partial class ClientStockTracker : DbContext
     {
-        public StockTrackerEntities()
-            : base("name=StockTrackerEntities")
+        public ClientStockTracker()
+            : base("name=ClientStockTracker")
         {
         }
     

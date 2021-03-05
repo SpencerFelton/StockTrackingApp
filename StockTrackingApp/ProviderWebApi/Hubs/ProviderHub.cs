aded282bc3aa087ae06f6bad539dc7b7a39975ff
Sinko <sinkoloi.mutambo@gmail.com>
2021-03-05 07:36:21 +0000

    started adding Signal R

---
+++ StockTrackingApp/ProviderWebApi/Hubs/ProviderHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProviderWebApi.Models;
+using System.Threading.Tasks;
+
+namespace ProviderWebApi.Hubs
+{
+    public class ProviderHub : Hub
+    {
+ 
+        public async Task GetUpdateForStockName(int id, string name)
+        {
+            Clients.All.addUpdatedStockNamesToPages(id, name);
+        }
+
+    }
+}

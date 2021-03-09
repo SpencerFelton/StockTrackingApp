293a8363169ba144ae2491342e5ca93732a6e073
Alex Robinson <allexjamesrobinson@gmail.com>
2021-03-09 15:24:03 +0000

    Re-installed all NuGet packages
    Some projects were using old versions of packages making testing and referencing them difficult. Re-installing the most recent versions brings everything up to date and in sync.

---
+++ StockTrackingApp/StockTracker/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StockTracker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StockTracker")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3449eecc-29a8-49de-a81a-359fe42fee06")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

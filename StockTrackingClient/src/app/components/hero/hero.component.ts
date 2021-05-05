8e6aea9f3bba13a64f447483bbbff4ceb8d88cf6
skiibot <ray-man100@hotmail.co.uk>
2021-05-05 10:17:43 +0100

    restructured scss to their respective components

--- StockTrackingClient/src/app/components/hero/hero.component.ts
+++ StockTrackingClient/src/app/components/hero/hero.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'hero',
-    templateUrl: './hero.component.html'
+    templateUrl: './hero.component.html',
+    styleUrls:['./hero.component.scss']
 })
 
 export class HeroComponent {

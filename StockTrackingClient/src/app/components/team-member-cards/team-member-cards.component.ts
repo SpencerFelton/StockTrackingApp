d6b30563fec677f21bf0c344e0b92da261ee490d
skiibot <ray-man100@hotmail.co.uk>
2021-05-12 11:07:45 +0100

    Got backend mostly working

--- StockTrackingClient/src/app/components/team-member-cards/team-member-cards.component.ts
+++ StockTrackingClient/src/app/components/team-member-cards/team-member-cards.component.ts
@@ -1,14 +1,40 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
     selector: 'team-member-cards',
     templateUrl: './team-member-cards.component.html',
-    styleUrls: ['./team-member-cards.component.scss']
+    styleUrls: ['./team-member-cards.component.scss'],
+    animations:[
+        trigger('fade', [
+            state('void', style({opacity: 0 })),
+            state('*', style({opacity: 100})),
+            transition('void <=> *', [
+                animate(200)
+            ])
+        ]),
+        trigger('fadefrominvis',[
+            state('active', style({backgroundColor:'var(--tertiary-colour)'})),
+            state('*', style({backgroundColor: '#FFFFFFFF'})),
+            transition('* <=> active', [
+                animate(200)
+            ])
+
+        ])
+        
+    ]
 })
 
 export class TeamMemberCardsComponent {
     
-    
+    showingDetails:boolean = false;
+
+    constructor(){}
+
+    onClicked(){
+        this.showingDetails = !this.showingDetails;
+        console.log("You clicked me! You really, really clicked me!");
+    }
 
 
 }
